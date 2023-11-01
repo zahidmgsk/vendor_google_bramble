53c07fe0e659804ad9945251ded77e2f0e75382d
author zahidmgsk <zahidmgsk@gmail.com>
2023-11-01 13:44:56 +0000

    remove conflicting packages DeviceIntelligenceNetworkPrebuilt DevicePersonalizationPrebuiltPixel2020
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- bramble-vendor.mk
+++ bramble-vendor.mk
@@ -456,8 +456,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
-    DevicePersonalizationPrebuiltPixel2020 \
     DiagMon \
     EuiccGoogle \
     HardwareInfo \
